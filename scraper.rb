bc9ccf39189d2493d6b1627a99b470149fd54203
Eric Tam <eric.tam@traceyanderic.com>
2017-04-11 09:46:41 +1000

    updated URL, updated info_url to use `a.href` data from the development application, updated comment_url to council’s email address, added additional output when saving records

--- scraper.rb
+++ scraper.rb
@@ -3,25 +3,27 @@ require 'mechanize'
 
 agent = Mechanize.new
 
-def scrape_page(page, url)
+def scrape_page(page, comment_url)
   table = page.at("table")
-  
+
   table.search("tr")[1..-1].each do |tr|
     day, month, year = tr.search("td")[3].inner_text.split(" ")
     month_i = Date::MONTHNAMES.index(month)
-    
+
     record = {
-      "info_url" => url,
-      "comment_url" => url,
+      "info_url" => tr.search("td a")[0].attributes['href'].to_s,
+      "comment_url" => comment_url,
       "council_reference" => tr.search("td")[0].inner_text,
       "description" => tr.search("td")[1].inner_text,
       "address" => tr.search("td")[2].inner_text + ", VIC",
       "on_notice_to" => Date.new(year.to_i, month_i, day.to_i).to_s,
       "date_scraped" => Date.today.to_s
     }
-    
+
     # Check if record already exists
     if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+      puts "Saving record " + record['council_reference'] + ", " + record['address']
+#      puts record
       ScraperWiki.save_sqlite(['council_reference'], record)
     else
       puts "Skipping already saved record " + record['council_reference']
@@ -29,7 +31,8 @@ def scrape_page(page, url)
   end
 end
 
-url = "http://www.cardinia.vic.gov.au/Page/Page.aspx?Page_Id=4421"
+url = "https://www.cardinia.vic.gov.au/info/20005/planning_and_building/287/advertised_planning_applications#section-2-advertised-permit-applications"
+comment_url = "mail@cardinia.vic.gov.au"
 page = agent.get(url)
 puts "Scraping page..."
-scrape_page(page, url)
+scrape_page(page, comment_url)
