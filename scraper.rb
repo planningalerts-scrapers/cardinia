9d189f5eb2eae1a046daea8f3457228d9d69d6e1
Jeremy West <jeremy@acaprojects.com>
2019-08-11 21:34:17 +1000

    re-enable scraperwiki gem

--- scraper.rb
+++ scraper.rb
@@ -1,28 +1,33 @@
-#require 'scraperwiki'
+require 'scraperwiki'
 require 'mechanize'
 
 agent = Mechanize.new
 
 def scrape_page(page, comment_url)
+  special = "?<>',?[]}{=-)(*&^%$#`~{}"
+  regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+
   table = page.at("table")
 
   table.search("tr")[1..-1].each do |tr|
     #day, month, year = tr.search("td")[3].inner_text.gsub(/[[:space:]]/, ' ').split(" ")
     #month_i = Date::MONTHNAMES.index(month)
 
+    address = tr.search("td")[2].inner_text + ", VIC"
+
     record = {
       "info_url" => tr.search("td a")[0].attributes['href'].to_s,
       "comment_url" => comment_url,
       "council_reference" => tr.search("td")[0].inner_text,
       "description" => tr.search("td")[1].inner_text,
-      "address" => tr.search("td")[2].inner_text + ", VIC",
+      "address" => address,
       #"on_notice_to" => Date.new(year.to_i, month_i, day.to_i).to_s,
       "date_scraped" => Date.today.to_s
     }
 
     puts "Saving record " + record['council_reference'] + ", " + record['address']
 #      puts record
-    ScraperWiki.save_sqlite(['council_reference'], record)
+    #ScraperWiki.save_sqlite(['council_reference'], record)
   end
 end
 
