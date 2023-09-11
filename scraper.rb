cdf9e98f71e2eb9f9107f8ca95b9cbce576b915c
Matthew Landauer <mlandauer@gmail.com>
2023-09-11 12:05:41 +1000

    Remove comment_url, handle blank on_notice_to and skip blank line

--- scraper.rb
+++ scraper.rb
@@ -3,20 +3,25 @@ require 'mechanize'
 
 agent = Mechanize.new
 
-def scrape_page(page, comment_url)
+def scrape_page(page)
   table = page.at("table")
 
   table.search("tr")[1..-1].each do |tr|
     day, month, year = tr.search("td")[3].inner_text.gsub(/[[:space:]]/, ' ').split(" ")
     month_i = Date::MONTHNAMES.index(month)
 
+    link = tr.search("td a")[0]
+    if link.nil?
+      puts "Skipping what is probably a blank line in the table"
+      next
+    end
+
     record = {
-      "info_url" => tr.search("td a")[0].attributes['href'].to_s,
-      "comment_url" => comment_url,
-      "council_reference" => tr.search("td")[0].inner_text,
-      "description" => tr.search("td")[1].inner_text,
-      "address" => tr.search("td")[2].inner_text + ", VIC",
-      "on_notice_to" => Date.new(year.to_i, month_i, day.to_i).to_s,
+      "info_url" => link.attributes['href'].to_s,
+      "council_reference" => tr.search("td")[0].inner_text.strip,
+      "description" => tr.search("td")[1].inner_text.strip,
+      "address" => tr.search("td")[2].inner_text.strip + ", VIC",
+      "on_notice_to" => (Date.new(year.to_i, month_i, day.to_i).to_s if day && month && year),
       "date_scraped" => Date.today.to_s
     }
 
@@ -27,7 +32,6 @@ def scrape_page(page, comment_url)
 end
 
 url = "https://www.cardinia.vic.gov.au/advertisedplanningapplications"
-comment_url = "mailto:mail@cardinia.vic.gov.au"
 page = agent.get(url)
 puts "Scraping page..."
-scrape_page(page, comment_url)
+scrape_page(page)
